e57724bd3777b8e62fe3fbd391266078ba6f351c
Johnny on Flame <johnnyonflame@hotmail.com>
2024-02-23 19:52:09 +0000

    KMSDRM: Force OpenGL ES by default.

--- src/video/kmsdrm/SDL_kmsdrmopengles.c
+++ src/video/kmsdrm/SDL_kmsdrmopengles.c
@@ -42,13 +42,12 @@ extern rga_info_t dst_info;
 
 void KMSDRM_GLES_DefaultProfileConfig(_THIS, int *mask, int *major, int *minor)
 {
+    // JohnnyonFlame: Also appropriate for our platform.
     /* if SDL was _also_ built with the Raspberry Pi driver (so we're
        definitely a Pi device), default to GLES2. */
-#if SDL_VIDEO_DRIVER_RPI
     *mask = SDL_GL_CONTEXT_PROFILE_ES;
     *major = 2;
     *minor = 0;
-#endif
 }
 
 int KMSDRM_GLES_LoadLibrary(_THIS, const char *path)
